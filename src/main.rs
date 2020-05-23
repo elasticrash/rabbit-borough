7ae6eb12f1c33c54238610df7e45ef34e91d6497
stefano <s.kouroupis@gmail.com>
2020-05-24 00:37:59 +0100

    basic working example

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
+use futures::future::{BoxFuture, FutureExt};
 use futures_executor::LocalPool;
-use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties, ExchangeKind};
+use lapin::types::FieldTable;
+use lapin::Channel;
+use lapin::{options::*, Connection, ConnectionProperties, ExchangeKind};
+use std::{thread, time};
 
 fn main() {
     std::env::set_var("RUST_LOG", "info");
-
-    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
-
+    let addr: &'static str = "amqp://127.0.0.1:5672/%2f";
     LocalPool::new().run_until(async {
-        let conn = Connection::connect(&addr, ConnectionProperties::default())
-            .await
-            .expect("connection error");
+        let channel = get_channel(addr).await;
 
-        println!("CONNECTED");
+        println!(
+            "[{}] channel status: {:?}",
+            line!(),
+            channel.status().state()
+        );
 
-        //receive channel
-        let channel = conn.create_channel().await.unwrap();
-
-        println!("[{}] state: {:?}", line!(), conn.status().state());
+        let exchange = channel
+            .exchange_declare(
+                "main.x",
+                ExchangeKind::Fanout,
+                ExchangeDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .wait();
+        println!("[{}] exchange status: {:?}", line!(), exchange);
 
         let queue = channel
             .queue_declare(
@@ -24,42 +33,21 @@ fn main() {
                 QueueDeclareOptions::default(),
                 FieldTable::default(),
             )
-            .await
-            .unwrap();
-        println!("[{}] state: {:?}", line!(), conn.status().state());
+            .await;
 
-        let exchange = channel
-            .exchange_declare(
-                "main.x",
-                ExchangeKind::Fanout,
-                ExchangeDeclareOptions {
-                    passive: false,
-                    durable: false,
-                    auto_delete: true,
-                    internal: false,
-                    nowait: true,
-                },
-                FieldTable::default(),
-            )
-            .await
-            .unwrap();
-
-        println!("[{}] state: {:?}", line!(), conn.status().state());
-        println!("declared exchange {:?}", exchange);
+        println!("[{}] queue status: {:?}", line!(), queue);
 
         let bind = channel
-            .exchange_bind(
+            .queue_bind(
                 "hello",
                 "main.x",
                 "rust",
-                ExchangeBindOptions { nowait: true },
+                QueueBindOptions { nowait: false },
                 FieldTable::default(),
             )
-            .await
-            .unwrap();
+            .wait();
 
-        println!("[{}] state: {:?}", line!(), conn.status().state());
-        println!("bind {:?}", bind);
+        println!("[{}] bind status: {:?}", line!(), bind);
 
         println!("will consume");
         let consumer = channel
@@ -71,7 +59,6 @@ fn main() {
             )
             .await
             .expect("basic_consume");
-        println!("[{}] state: {:?}", line!(), conn.status().state());
 
         for delivery in consumer {
             println!("received message: {:?}", delivery);
@@ -83,4 +70,43 @@ fn main() {
             }
         }
     })
+}
+
+async fn get_channel(addr: &'static str) -> Channel {
+    let conn = get_connection(&addr, 0).await;
+    println!(
+        "[{}] connection state: {:?}",
+        line!(),
+        conn.status().state()
+    );
+
+    return match conn.create_channel().await {
+        Ok(ch) => ch,
+        Err(why) => panic!("{}", why),
+    };
+}
+
+/// Returns a Connection, retries x times
+fn get_connection(addr: &'static str, retry: u64) -> BoxFuture<'static, Connection> {
+    return Box::pin(
+        async move {
+            let con_promise = Connection::connect(&addr, ConnectionProperties::default());
+            let conn_res = con_promise.await;
+            let connection = match conn_res {
+                Ok(c) => c,
+                Err(why) => {
+                    println!("{}", why);
+                    if retry > 5 {
+                        panic!("maximum retries reached");
+                    }
+                    let hibernate = time::Duration::from_millis(retry * 100);
+                    thread::sleep(hibernate);
+                    let c = get_connection(addr, retry + 1);
+                    c.await
+                }
+            };
+            connection
+        }
+        .boxed(),
+    );
 }
