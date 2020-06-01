382dced27e5f1ecb4bd0e41eccff4667cba168f3
stefano <s.kouroupis@gmail.com>
2020-06-01 23:53:55 +0100

    connection resiliency

--- src/consumer/consumer.rs
+++ src/consumer/consumer.rs
@@ -1,5 +1,8 @@
+use crate::configuration::config_model::JSONConfiguration;
 use crate::consumer::handler_message_result::action_result;
 use crate::consumer::handler_message_result::HandleMessageResult;
+use crate::consumer::setup::get_channel;
+use crate::consumer::setup::setup_consumer;
 use lapin::message::Delivery;
 use lapin::options::BasicConsumeOptions;
 use lapin::types::FieldTable;
@@ -19,10 +22,32 @@ pub async fn create_consumer(queue_name: &str, channel: &Channel) -> Consumer {
     return consumer;
 }
 
-pub async fn consume(consumer: Consumer, handler: &dyn Fn(&Delivery) -> HandleMessageResult) {
-    for delivery in consumer {
-        if let Ok((channel, delivery)) = delivery {
-            action_result(handler(&delivery), &channel, delivery.delivery_tag).await;
+pub async fn consume(
+    config: &JSONConfiguration,
+    handler: &dyn Fn(&Delivery) -> HandleMessageResult,
+) {
+    loop {
+        let model = setup_consumer(
+            config.connection.clone(),
+            config.binding.clone(),
+            config.declare.clone(),
+        )
+        .await;
+        println!(
+            "[{}] channel status: {:?}",
+            line!(),
+            model.channel.status().state()
+        );
+        let consumer = create_consumer(&config.binding.queue, &model.channel).await;
+        for message in consumer {
+            match message {
+                Ok((channel, delivery)) => {
+                    action_result(handler(&delivery), &channel, delivery.delivery_tag).await;
+                }
+                Err(why) => {
+                    println!("[{}] channel status: {:?}", line!(), why);
+                }
+            };
         }
     }
 }
